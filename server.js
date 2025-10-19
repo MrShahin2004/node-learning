e5be8015204f959ece5ea58d86c1af02ffbfb7c4
MrShahin2004 <hashmyshahin@gmail.com>
2025-10-19 19:54:55 +0330

    Rendered a simple HTML page.

--- server.js
+++ server.js
@@ -5,6 +5,12 @@ const HTTP = require('http');
 let Server = HTTP.createServer((req,
                                 res) => {
     console.log(req.url, req.method, req.headers);
+    res.setHeader("Content-Type", "text/html");
+    res.write("<!DOCTYPE html>");
+    res.write("<head><title>My First SSR Response</title></head>");
+    res.write("<body><h1>Hello from Node.js</h1></body>")
+    res.write("</html>");
+    res.end();
 });
 
 Server.listen(3000);
