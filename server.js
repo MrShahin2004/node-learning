149548ffda785617144504b44a77809f737dab78
MrShahin2004 <hashmyshahin@gmail.com>
2025-10-19 19:03:43 +0330

    Created my first simple server.

--- server.js
+++ server.js
@@ -1,3 +1,10 @@
 "use strict";
 
 const HTTP = require('http');
+
+let Server = HTTP.createServer((req,
+                                res) => {
+    console.log("HTTP server started!");
+});
+
+Server.listen(3000);
