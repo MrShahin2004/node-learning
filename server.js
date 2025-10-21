b96f6146d39489261ac99fbef0571d14a3ecabb8
MrShahin2004 <hashmyshahin@gmail.com>
2025-10-21 15:58:28 +0330

    Huh?

--- server.js
+++ server.js
@@ -1,15 +1,3 @@
 "use strict";
 
 
-let Server = HTTP.createServer((req,
-                                res) => {
-    console.log(req.url, req.method, req.headers);
-    res.setHeader("Content-Type", "text/html");
-    res.write("<!DOCTYPE html>");
-    res.write("<head><title>My First SSR Response</title></head>");
-    res.write("<body><h1>Hello from Node.js</h1></body>")
-    res.write("</html>");
-    res.end();
-});
-
-Server.listen(3000);
