3ac0773961f3769db95dae50ad8ee6ef11ed8930
MrShahin2004 <hashmyshahin@gmail.com>
2025-10-21 17:54:51 +0330

    Made it more readable.

--- server.js
+++ server.js
@@ -7,10 +7,19 @@ const server = http.createServer(
         console.log(req.url, req.method, req.headers);
         // process.exit();
         res.setHeader('Content-Type', 'text/html');
-        res.write('<html lang="en-US">');
-        res.write('<head><title>My First Page</title></head>');
-        res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
-        res.write('</html>');
+        res.write(`<!doctype html>`);
+        res.write(`<html lang="en-US">`);
+        res.write(`
+            <head>
+                <title>My First Page</title>
+            </head>
+        `);
+        res.write(`
+            <body>
+                <h1>Hello from my Node.js Server!</h1>
+            </body>
+        `);
+        res.write(`</html>`);
         res.end();
     });
 
