de21897ef0dc6c578b910a93742667c7d2319017
MrShahin2004 <hashmyshahin@gmail.com>
2025-10-25 16:51:13 +0330

    Removed this part from here to change our logic.

--- server.js
+++ server.js
@@ -7,59 +7,6 @@ const Server = HTTP.createServer(
     (req, res) => {
         const URL = req.url;
         const Method = req.method;
-        if (URL === "/") {
-            res.setHeader('Content-Type', 'text/html');
-            res.write(`<!doctype html>`);
-            res.write(`<html lang="en-US">`);
-            res.write(`
-                <head>
-                    <title>Enter Message</title>
-                </head>
-            `);
-            res.write(`
-                <body>
-                    <form action="/message" method="post">
-                        <input name="message" type="text"/>
-                        <button type="submit">Send</button>
-                    </form>
-                </body>
-            `);
-            res.write(`</html>`);
-            return res.end();
-        }
-
-        if (URL === "/message" && Method === "POST") {
-            let Body = [];
-            req.on("data", (chunk) => {
-                console.log(chunk);
-                Body.push(chunk);
-            });
-            return req.on("end", () => {
-                let ParsedBody = Buffer.concat(Body).toString();
-                let Message = ParsedBody.split("=")[1];
-                FS.writeFile("message.txt", Message, (error) => {
-                    res.statusCode = 302;
-                    res.setHeader("Location", "/");
-                    return res.end();
-                });
-            });
-        }
-
-        res.setHeader('Content-Type', 'text/html');
-        res.write(`<!doctype html>`);
-        res.write(`<html lang="en-US">`);
-        res.write(`
-            <head>
-                <title>My First Page</title>
-            </head>
-        `);
-        res.write(`
-            <body>
-                <h1>Hello from my Node.js Server!</h1>
-            </body>
-        `);
-        res.write(`</html>`);
-        res.end();
     });
 
 Server.listen(3001);
