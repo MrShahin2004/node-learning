4f9f5e012aaff17734ff623da874287796937408
MrShahin2004 <hashmyshahin@gmail.com>
2025-10-27 00:27:15 +0330

    Modified the file.

--- server.js
+++ server.js
@@ -4,9 +4,23 @@ const Express = require("express");
 const BodyParser = require("body-parser");
 
 const App = Express();
-App.use((req,res,next)=>{
+App.use("/add-product", (req, res, next) => {
     res.send(`
-        <h1>Hey, talking to you from Express.js!</h1>
+        <form action="/product" method="post">
+            <input name="title" type="text">
+            <button type="submit">Add Product</button>
+        </form>
+    `);
+});
+
+App.use("/product", (req, res, next) => {
+    console.log(req.body);
+    res.redirect("/");
+});
+
+App.use("/", (req, res, next) => {
+    res.send(`
+        <h1>Hey, this is Express.js!</h1>
     `);
 });
 
