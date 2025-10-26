df2ab04a9ea880dee622676971647dbaa37b642f
MrShahin2004 <hashmyshahin@gmail.com>
2025-10-26 20:47:53 +0330

    Sent a sample template.

--- server.js
+++ server.js
@@ -4,6 +4,20 @@ const HTTP = require("http");
 const Express = require("express");
 
 const App = Express();
+App.use((req,res,next)=>{
+    res.send(`
+        <h1>Hey, talking to you from Express.js!</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A amet aperiam asperiores at autem blanditiis corporis culpa cupiditate dignissimos, dolorum ducimus facere fuga harum in inventore itaque labore maxime minima provident quis ratione soluta suscipit tempora, tenetur voluptates voluptatibus voluptatum! Consectetur debitis earum itaque mollitia perferendis, qui quod repudiandae sapiente! Animi deleniti dolores excepturi fuga, maiores mollitia, natus officia quaerat, ratione sed suscipit tempore voluptates. Amet aperiam, autem beatae commodi ex mollitia nemo officia pariatur reiciendis tempore! Doloribus earum iusto labore mollitia nemo odio, optio ut. Expedita fuga incidunt minima nisi quos repudiandae ullam vitae! Ducimus itaque suscipit temporibus voluptatibus.</p>
+        <ul>
+            <li>Home</li>
+            <li>Products</li>
+            <li>Giveaway</li>
+            <li>Support</li>
+            <li>Contact</li>
+            <li>About</li>
+        </ul>
+    `);
+});
 
 const Server = HTTP.createServer(App);
 
