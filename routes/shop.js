b342b43f20f4eb58f3f6e38144a78cd49a97407d
MrShahin2004 <hashmyshahin@gmail.com>
2025-10-28 17:23:22 +0330

    Did the same thing to this route.

---
+++ routes/shop.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const Express = require('express');
+const Router = Express.Router();
+
+Router.get("/",
+    (req, res, next) => {
+        res.send(`
+        <h1>Hey, this is Express.js!</h1>
+    `);
+    });
+
+module.exports = Router;
