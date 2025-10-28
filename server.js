dbcbc5de918155fef7a6e6de6f6772070fda4b8c
MrShahin2004 <hashmyshahin@gmail.com>
2025-10-28 17:24:03 +0330

    Then used it.

--- server.js
+++ server.js
@@ -8,12 +8,6 @@ const BodyParser = require("body-parser");
 const App = Express();
 App.use(BodyParser.urlencoded({extended: false}));
 App.use(AdminRoutes);
-
-App.get("/",
-    (req, res, next) => {
-        res.send(`
-        <h1>Hey, this is Express.js!</h1>
-    `);
-    });
+App.use(ShopRoute);
 
 App.listen(3001);
