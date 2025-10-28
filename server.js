9a0cccd1d5d3b9e078a2af8ef0c16d8725c50e14
MrShahin2004 <hashmyshahin@gmail.com>
2025-10-28 17:02:18 +0330

    Cut these routes from here.

--- server.js
+++ server.js
@@ -6,22 +6,6 @@ const BodyParser = require("body-parser");
 const App = Express();
 App.use(BodyParser.urlencoded({extended: false}));
 
-App.get("/add-product",
-    (req, res, next) => {
-        res.send(`
-        <form action="/product" method="post">
-            <input name="title" type="text">
-            <button type="submit">Add Product</button>
-        </form>
-    `);
-    });
-
-App.post("/product",
-    (req, res, next) => {
-        console.log(req.body);
-        res.redirect("/");
-    });
-
 App.get("/",
     (req, res, next) => {
         res.send(`
