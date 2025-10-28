f00bc4ffb64a70b9b1f44f992097b379b6c1c3b1
MrShahin2004 <hashmyshahin@gmail.com>
2025-10-28 17:26:18 +0330

    Added some comment.

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 "use strict";
 
+// Imported the necessary modules
 const Express = require("express");
 const AdminRoutes = require("./routes/admin");
 const ShopRoute = require("./routes/shop");
 const BodyParser = require("body-parser");
 
+// Created an app.
 const App = Express();
+
+// Used the "body-parser" module.
 App.use(BodyParser.urlencoded({extended: false}));
+
+// Used my created modules.
 App.use(AdminRoutes);
 App.use(ShopRoute);
 
+// Initiated the server
 App.listen(3001);
