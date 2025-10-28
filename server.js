ab0d6fbaabc40d82293ffb70fcb2a6602c1507d0
MrShahin2004 <hashmyshahin@gmail.com>
2025-10-28 16:35:47 +0330

    Changed the function names but their functionality is the same.

--- server.js
+++ server.js
@@ -6,24 +6,27 @@ const BodyParser = require("body-parser");
 const App = Express();
 App.use(BodyParser.urlencoded({extended: false}));
 
-App.use("/add-product", (req, res, next) => {
-    res.send(`
+App.get("/add-product",
+    (req, res, next) => {
+        res.send(`
         <form action="/product" method="post">
             <input name="title" type="text">
             <button type="submit">Add Product</button>
         </form>
     `);
-});
+    });
 
-App.post("/product", (req, res, next) => {
-    console.log(req.body);
-    res.redirect("/");
-});
+App.post("/product",
+    (req, res, next) => {
+        console.log(req.body);
+        res.redirect("/");
+    });
 
-App.use("/", (req, res, next) => {
-    res.send(`
+App.get("/",
+    (req, res, next) => {
+        res.send(`
         <h1>Hey, this is Express.js!</h1>
     `);
-});
+    });
 
 App.listen(3001);
