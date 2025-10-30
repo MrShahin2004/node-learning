09892f6529d21559889a3d3881798f3a0b59834b
MrShahin2004 <hashmyshahin@gmail.com>
2025-10-30 15:14:12 +0330

    A JS file to run PowerShell scripts.

---
+++ pwsh-run.js
@@ -0,0 +1,21 @@
+"use strict";
+
+const Path = require("path");
+const ChildProcess = require('child_process');
+
+const PathToPSEngine = Path.join("C:", "Program Files", "PowerShell", "7", "pwsh.exe");
+const PathToPSFile = Path.join("D:", "Front-end Programming", "JS Projects",
+    "My Projects", "node-learning", "command.ps1");
+
+ChildProcess.exec(`"${PathToPSEngine}" -ExecutionPolicy Bypass -File "${PathToPSFile}"`,
+    (error, stdout, stderr) => {
+        if (error) {
+            console.error(error);
+        }
+
+        if (stderr) {
+            console.error(stderr);
+        }
+
+        console.log(stdout);
+    });
