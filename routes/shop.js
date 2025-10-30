0c718bab6f54c5431a87c060c78ed20cf89e1912
MrShahin2004 <hashmyshahin@gmail.com>
2025-10-30 15:47:35 +0330

    Imported the admin data.

--- routes/shop.js
+++ routes/shop.js
@@ -4,6 +4,7 @@ const Path = require("path");
 const Express = require('express');
 const Router = Express.Router();
 const RootDir = require("../utils/path");
+const AdminData = require("./admin");
 
 Router.get("/", (req, res) => {
     res.sendFile(Path.join(RootDir, "views", "shop.html"));
