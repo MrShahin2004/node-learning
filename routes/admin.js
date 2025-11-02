cbf1aa98e997219b2cce218261f8f694158cb351
MrShahin2004 <hashmyshahin@gmail.com>
2025-11-02 18:15:44 +0330

    Was passing wrongly.

--- routes/admin.js
+++ routes/admin.js
@@ -5,12 +5,8 @@ const Express = require("express");
 const ProductsController = require("../controllers/products");
 const Router = Express.Router();
 
-Router.get("/add-product",
-    (req, res) => {
-        res.render("add-product", ProductsController.GetAddProduct);
-    });
+Router.get("/add-product", ProductsController.GetAddProduct);
 
 Router.post("/add-product", ProductsController.PostAddProduct);
 
 exports.router = Router;
-exports.products = ProductsArray;
