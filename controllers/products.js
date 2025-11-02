420ada0a06d347a9fecc4c610413063ce46f4859
MrShahin2004 <hashmyshahin@gmail.com>
2025-11-02 18:15:24 +0330

    Put it here.

--- controllers/products.js
+++ controllers/products.js
@@ -14,3 +14,14 @@ exports.PostAddProduct = (req, res) => {
     ProductsArray.push({title: req.body.title});
     res.redirect("/");
 }
+
+exports.GetProducts = (req, res, next) => {
+    res.render('shop', {
+        prods: ProductsArray,
+        pageTitle: 'Shop',
+        path: '/',
+        hasProducts: ProductsArray.length > 0,
+        activeShop: true,
+        productCSS: true
+    });
+};
