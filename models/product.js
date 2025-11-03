2b4430e731d8dbb1bcd3acc8dd59b50d900bb825
MrShahin2004 <hashmyshahin@gmail.com>
2025-11-03 19:51:19 +0330

    Fixed the asynchronous issue.

--- models/product.js
+++ models/product.js
@@ -3,16 +3,45 @@
 const FS = require("fs");
 const Path = require("path");
 
+const JSONFilePath = Path.join(Path.dirname(require.main.filename), "data", "products.json");
+
 module.exports = class Product {
     constructor(title) {
         this.title = title;
     }
 
     save() {
-        const p = Path.join(Path.dirname(process.mainModule.filename), "data", "products.json");
+        FS.readFile(JSONFilePath, (error, content) => {
+            let products = [];
+            if (!error && content && content.length) {
+                try {
+                    products = JSON.parse(content);
+                } catch (error) {
+                    console.error("Failed to parse the JSON value. Error log: ", error);
+                }
+            }
+            products.push(this);
+
+            FS.writeFile(JSONFilePath, JSON.stringify(products, null, 2), (error) => {
+                if (error) {
+                    console.error(error);
+                }
+            });
+        });
     }
 
-    static fetchAll() {
-        return ProductsArray;
+    static fetchAll(callback) {
+        FS.readFile(JSONFilePath, (error, content) => {
+            if (error || !content || !content.length) {
+                return callback([]);
+            } else {
+                try {
+                    callback(JSON.parse(content));
+                } catch (e) {
+                    console.error("Failed to return the data. Error log: ", e);
+                    callback([]);
+                }
+            }
+        });
     }
 }
