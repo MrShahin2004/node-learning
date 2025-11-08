40c3ffb6247a52d105657269cbac0113f398005c
MrShahin2004 <hashmyshahin@gmail.com>
2025-11-08 16:00:01 +0330

    Also new lines

--- controllers/error.js
+++ controllers/error.js
@@ -1,5 +1,8 @@
 "use strict";
 
 exports.Get404 = (req, res) => {
-    res.status(404).render("not-found", {pageTitle: "404 Not Found", path: req.path});
+    res.status(404).render("not-found", {
+        pageTitle: "404 Not Found",
+        path: req.path
+    });
 }
