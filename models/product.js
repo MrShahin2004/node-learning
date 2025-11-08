7366e4d9acef16374d6479d939a2e49cf175448d
MrShahin2004 <hashmyshahin@gmail.com>
2025-11-08 16:57:54 +0330

    Created a function to fetch the data from file.

--- models/product.js
+++ models/product.js
@@ -5,6 +5,21 @@ const Path = require("path");
 
 const JSONFilePath = Path.join(Path.dirname(require.main.filename), "data", "products.json");
 
+const GetProductsFromFile = (callback) => {
+    FS.readFile(JSONFilePath, (error, content) => {
+        if (error || !content || !content.length) {
+            return callback([]);
+        } else {
+            try {
+                return callback(JSON.parse(content));
+            } catch (e) {
+                console.error("Failed to return the data. Error log: ", e);
+                return callback([]);
+            }
+        }
+    });
+}
+
 module.exports = class Product {
     constructor(title) {
         this.title = title;
