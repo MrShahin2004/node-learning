ecb5bb08d799ffd352be40af10608b8bb458699d
MrShahin2004 <hashmyshahin@gmail.com>
2025-11-09 21:57:50 +0330

    Created a new file for the controllers of the main shop pages.

---
+++ controllers/shop.js
@@ -0,0 +1,37 @@
+"use strict";
+
+const Product = require("../models/product");
+
+exports.GetIndex = (req, res, next) => {
+    Product.fetchAll((products) => {
+        res.render("shop/index", {
+            prods: products,
+            pageTitle: "Shop",
+            path: "/"
+        });
+    });
+}
+
+exports.GetProducts = (req, res, next) => {
+    Product.fetchAll((products) => {
+        res.render("shop/product-list", {
+            prods: products,
+            pageTitle: "All Products",
+            path: "/products",
+        });
+    });
+};
+
+exports.GetCart = (req, res, next) => {
+    res.render("shop/cart", {
+        path: "/cart",
+        pageTitle: "Your Cart"
+    });
+}
+
+exports.GetCheckout = (req, res, next) => {
+    res.render("shop/checkout", {
+        path: "/checkout",
+        pageTitle: "Checkout"
+    });
+}
