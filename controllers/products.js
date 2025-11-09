e1447f4c440221ab77a6275173ec83bed4d0be1d
MrShahin2004 <hashmyshahin@gmail.com>
2025-11-09 20:27:02 +0330

    No idea why this got some spaces.

--- controllers/products.js
+++ controllers/products.js
@@ -19,14 +19,14 @@ exports.PostAddProduct = (req, res) => {
 }
 
 exports.GetProducts = (req, res, next) => {
-  Product.fetchAll((products) => {
-    res.render("shop/product-list", {
-      prods: products,
-      pageTitle: "Shop",
-      path: "/",
-      hasProducts: products.length > 0,
-      activeShop: true,
-      productCSS: true
+    Product.fetchAll((products) => {
+        res.render("shop/product-list", {
+            prods: products,
+            pageTitle: "Shop",
+            path: "/",
+            hasProducts: products.length > 0,
+            activeShop: true,
+            productCSS: true
+        });
     });
-  });
 };
