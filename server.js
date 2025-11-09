8e059025e11b254a39b219b0b5124f5ee37524ad
MrShahin2004 <hashmyshahin@gmail.com>
2025-11-09 21:57:25 +0330

    Deleted the unused code.

--- server.js
+++ server.js
@@ -14,7 +14,6 @@ App.set("views", "views");
 
 const AdminRoutes = require("./routes/admin");
 const ShopRoute = require("./routes/shop");
-const CartRoute = require("./routes/cart");
 
 // Used the "body-parser" module.
 App.use(BodyParser.urlencoded({extended: false}));
@@ -23,7 +22,6 @@ App.use(Express.static(Path.join(__dirname, "public")));
 // Used my created modules.
 App.use("/admin", AdminRoutes.router); // Can determine what segment should the paths in the module be in.
 App.use(ShopRoute);
-App.use(CartRoute.router);
 
 // Added a 404 status page.
 App.use(ErrorController.Get404);
