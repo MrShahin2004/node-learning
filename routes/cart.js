255dffa36552e0ab3d78636bff0ce9a6789c1d8d
MrShahin2004 <hashmyshahin@gmail.com>
2025-11-09 21:34:11 +0330

    Removed this redundant controller.

--- routes/cart.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-const Express = require("express");
-const Router = Express.Router();
-const CartController = require("../controllers/products");
-
-Router.get("/cart", CartController.GoToCart);
-
-exports.router = Router;
