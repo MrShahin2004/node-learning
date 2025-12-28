a648ca464e7da3aca0992f693f5ad947a670a653
MrShahin2004 <hashmyshahin@gmail.com>
2025-12-28 19:49:05 +0330

    Reading the JSON file if exists and assigning the values.

--- models/cart.js
+++ models/cart.js
@@ -6,6 +6,13 @@ const JSONFilePathForCart = Path.join(Path.dirname(require.main.filename), "data
 module.exports = class Cart {
     static addProduct() {
         // Fetch the previous cart
+        FSE.readFile(JSONFilePathForCart, (error, content) => {
+            let Cart = { products: [], totalPrice: 0 };
+
+            if (!error) {
+                Cart = JSON.parse(content);
+            }
+        });
         // Analyze the cart --> Find the existing product
         // Add new product / Increase quantity
     }
