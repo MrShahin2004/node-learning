4a231bfcb25b6ca3a834f3a8c99e114f6145fcba
MrShahin2004 <hashmyshahin@gmail.com>
2025-12-28 21:24:48 +0330

    Completed the cart logic.

--- models/cart.js
+++ models/cart.js
@@ -8,32 +8,30 @@ module.exports = class Cart {
     static addProduct(id, productPrice) {
         // Fetch the previous cart
         FSE.readFile(JSONFilePathForCart, (error, content) => {
-            let Cart = { products: [], totalPrice: 0 };
+            let CartProperties = { products: [], totalPrice: 0 };
 
             if (!error) {
-                Cart = JSON.parse(content);
+                CartProperties = JSON.parse(content);
             }
+
+            // Analyze the cart --> Find the existing product
+            let ExistingProductIndex = CartProperties.products.findIndex(product => product.id === id);
+            let ExistingProduct = CartProperties.products[ExistingProductIndex];
+            let UpdatedProduct;
+
+            // Add new product / Increase quantity
+            if (ExistingProduct) {
+                UpdatedProduct = { ...ExistingProduct };
+                UpdatedProduct.quantity = UpdatedProduct.quantity + 1;
+                CartProperties.products = [...CartProperties.products];
+                CartProperties.products[ExistingProductIndex] = UpdatedProduct;
+            } else {
+                UpdatedProduct = { id: id, quantity: 1 };
+                CartProperties.products = [...CartProperties.products, UpdatedProduct];
+            }
+            CartProperties.totalPrice += +productPrice;
+
+            FSE.writeFile(JSONFilePathForCart, JSON.stringify(CartProperties), "utf-8");
         });
-
-        // Analyze the cart --> Find the existing product
-        let ExistingProductIndex = Cart.products.findIndex((product) => {
-            return product.id === id;
-        });
-        let ExistingProduct = Cart.products[ExistingProductIndex];
-        let UpdatedProduct;
-
-        // Add new product / Increase quantity
-        if (ExistingProduct) {
-            UpdatedProduct = { ...ExistingProduct };
-            UpdatedProduct.quantity = UpdatedProduct.quantity + 1;
-            Cart.products = [...Cart.products];
-            Cart.products[ExistingProductIndex] = UpdatedProduct;
-        } else {
-            UpdatedProduct = { id: id, quantity: 1 };
-            Cart.products = [...Cart.products, UpdatedProduct];
-        }
-        Cart.totalPrice += productPrice;
-
-        FSE.writeFile(JSONFilePathForCart, JSON.stringify(Cart), "utf-8");
     }
 }
